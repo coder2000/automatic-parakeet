244537c5bde3bb31e6812d43865226e8a63618fe
Dieter Lunn <coder2000@gmail.com>
2025-07-06 14:48:14 -0300

    feat: implement N+1 query detection in non-production environments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,17 @@ class ApplicationController < ActionController::Base
 
   before_action :set_locale
 
+  unless Rails.env.production?
+    around_action :n_plus_one_detection
+
+    def n_plus_one_detection
+      Prosopite.scan
+      yield
+    ensure
+      Prosopite.finish
+    end
+  end
+
   private
 
   # Set locale from params, user, or default
