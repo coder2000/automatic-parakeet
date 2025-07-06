ac6d298f7c39a1fa6a3d8c4de8106d1f98479251
Dieter Lunn <coder2000@gmail.com>
2025-07-06 15:23:35 -0300

    feat: add ActiveAdmin registration for Tool with permit parameters and form setup

---
+++ app/admin/tool.rb
@@ -0,0 +1,22 @@
+ActiveAdmin.register Tool do
+  permit_params :name
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  filter :name
+  filter :created_at
+
+  form do |f|
+    f.inputs do
+      f.input :name
+    end
+    f.actions
+  end
+end
