ab73880b3122cb59cf5f9a843cb6c90fd332f7b9
Dieter Lunn <coder2000@gmail.com>
2025-07-08 08:56:47 -0300

    fix: update Procfile.dev and assets initializer for consistency and additional asset precompilation

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,10 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 
+# Precompile additional assets for ActiveAdmin
+Rails.application.config.assets.precompile += %w[active_admin.css active_admin.js active_admin.scss]
+
 # Ensure JavaScript files are served with correct MIME type
 Rails.application.config.assets.configure do |env|
-  env.register_mime_type 'text/javascript', extensions: ['.js']
+  env.register_mime_type "text/javascript", extensions: [".js"]
 end
