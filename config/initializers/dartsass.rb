8fa911e203b40dfd4d71efa2984d0b7e37655351
Dieter Lunn <coder2000@gmail.com>
2025-07-08 22:36:15 -0300

    feat: remove dartsass initializer as part of asset pipeline cleanup

--- config/initializers/dartsass.rb
+++
@@ -1,4 +0,0 @@
-# config/initializers/dartsass.rb
-Rails.application.config.dartsass.builds = {
-  "#{Rails.root}/app/assets/stylesheets/active_admin.scss" => "#{Rails.root}/app/assets/builds/active_admin.css"
-}
