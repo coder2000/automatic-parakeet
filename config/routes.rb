f3d56f7f6860e467b39f0c8cf03b6b429d136737
Dieter Lunn <coder2000@gmail.com>
2025-08-03 22:25:44 -0300

    fix: remove unused routes for following and ratings in users resources

--- config/routes.rb
+++ config/routes.rb
@@ -25,8 +25,6 @@ Rails.application.routes.draw do
     resources :users, only: [:show, :edit, :update] do
       member do
         get :games
-        get :following
-        get :ratings
       end
     end
 
