9f7897acac15133972e7e741e3f67a7b6be47ddc
Dieter Lunn <coder2000@gmail.com>
2025-08-06 10:21:54 -0300

    Refactor follow and unfollow actions to use a centralized error rendering method

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -4,6 +4,7 @@ class FollowingsController < ApplicationController
 
   def create
     @following = current_user.followings.build(game: @game)
+    @style = style_param
 
     if @following.save
       @following.create_activity(:follow)
@@ -13,16 +14,13 @@ class FollowingsController < ApplicationController
         format.json { render json: {status: "followed", followers_count: @game.followings.count} }
       end
     else
-      respond_to do |format|
-        format.html { redirect_to @game, alert: "Unable to follow this game." }
-        format.turbo_stream { render turbo_stream: turbo_stream.replace("follow_button_#{@game.id}", partial: "shared/follow_button", locals: {game: @game}) }
-        format.json { render json: {error: @following.errors.full_messages}, status: :unprocessable_entity }
-      end
+      render_error("Unable to follow this game.")
     end
   end
 
   def destroy
     @following = current_user.followings.find_by(game: @game)
+    @style = style_param
 
     if @following&.destroy
       respond_to do |format|
@@ -31,11 +29,7 @@ class FollowingsController < ApplicationController
         format.json { render json: {status: "unfollowed", followers_count: @game.followings.count} }
       end
     else
-      respond_to do |format|
-        format.html { redirect_to @game, alert: "Unable to unfollow this game." }
-        format.turbo_stream { render turbo_stream: turbo_stream.replace("follow_button_#{@game.id}", partial: "shared/follow_button", locals: {game: @game}) }
-        format.json { render json: {error: "Following not found"}, status: :not_found }
-      end
+      render_error("Unable to follow this game.")
     end
   end
 
@@ -47,6 +41,18 @@ class FollowingsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to games_path, alert: "Game not found." }
       format.json { render json: {error: "Game not found"}, status: :not_found }
+    end
+  end
+
+  def style_param
+    (params[:style] || default).to_sym
+  end
+
+  def render_error(message)
+    respond_to do |format|
+      format.html { redirect_to @game, alert: message }
+      format.turbo_stream { render turbo_stream: turbo_stream.replace("follow_button_#{@game.id}", partial: "shared/follow_button", locals: {game: @game}) }
+      format.json { render json: {error: @following.errors.full_messages}, status: :unprocessable_entity }
     end
   end
 end
