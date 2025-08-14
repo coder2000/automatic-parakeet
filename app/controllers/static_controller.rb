78f40c4bcf9a12794b53986f503cfe980cd10830
FreankOS <31592138+FreankOS@users.noreply.github.com>
2025-08-14 09:50:38 +0200

    Added pages Developers & About

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,13 @@
 class StaticController < ApplicationController
+
+  def about
+    render layout: "application"
+  end
+  
+  def developers
+    render layout: "application"
+  end
+  
   def faq
     render layout: "application"
   end
