0f6a1d0bff5060edd10d01a06e0a6792dc36e134
Dieter Lunn <coder2000@gmail.com>
2025-08-15 16:43:42 -0300

    Remove unnecessary blank lines in StaticController

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,13 +1,12 @@
 class StaticController < ApplicationController
-
   def about
     render layout: "application"
   end
-  
+
   def developers
     render layout: "application"
   end
-  
+
   def faq
     render layout: "application"
   end
